edba922d1ca21c76b2b72fe4eeaafe3b84d78bc2
n-kats <n-kats19890214@hotmail.co.jp>
2025-04-09 21:21:52 +0900

    使ってみた問題を反映

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,3 +1,7 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__name__)
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
